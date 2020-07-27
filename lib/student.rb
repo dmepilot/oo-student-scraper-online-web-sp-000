89a5fb2cc183c33474ac0199afc6260e471d0cde
Dean Erickson <dmepilot@yahoo.com>
2020-07-27 15:03:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,15 @@ class Student
   @@all = []
 
   def initialize(student_hash)
+    @name = name
+    @location = location
+    @twitter = twitter
+    @linkedin = linkedin
+    @github = github
+    @blog = blog
+    @profile_quote = profile_quote
+    @bio = bio
+    @profile_url = profile_url
     
   end
 
