288d831d2d71af94ff802663ae3cbabda74f8d5c
Dean Erickson <dmepilot@yahoo.com>
2020-07-27 15:25:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,15 +5,6 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    # @name = name
-    # @location = location
-    # @twitter = twitter
-    # @linkedin = linkedin
-    # @github = github
-    # @blog = blog
-    # @profile_quote = profile_quote
-    # @bio = bio
-    # @profile_url = profile_url
 
     student_hash.each{|key, value| self.send(("#{key}="), value)}
     @@all << self
